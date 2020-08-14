1a260491f033da1151d9e9ca9b92efb7b96d1663
jianggengwu <767219520@qq.com>
2020-08-14 11:20:08 +0800

    1

--- lib/src/commonwidgets/stateful_widget_cell_plus.dart
+++ lib/src/commonwidgets/stateful_widget_cell_plus.dart
@@ -1,79 +1,54 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:saber_package/src/utils/common.dart';
 
 class StatefulWidgetCellPlus extends StatefulWidget {
   StatefulWidgetController controller;
   Widget child;
-  GestureTapCallback onTap;
-  bool disableBack;
-  bool hide;
-  double opacity;
-  WidgetBuilder widgetBuilder;
+//  Key key;
 
   StatefulWidgetCellPlus(
-      {Key key,
-      this.controller,
-      this.child,
-      this.widgetBuilder,
-      this.disableBack = false,
-      this.opacity = 1,
-      this.hide = false})
-      : super(key: key);
+//    this.key,
+      @required this.controller,
+      @required this.child,
+      );
 
   @override
   _StatefulWidgetCellStatePlus createState() => _StatefulWidgetCellStatePlus();
 }
 
-class _StatefulWidgetCellStatePlus extends State<StatefulWidgetCellPlus> {
+class _StatefulWidgetCellStatePlus extends State<StatefulWidgetCellPlus>  with WidgetsBindingObserver {
   WidgetsBinding widgetsBinding;
-  VoidCallback listener;
-  bool _forcedRefresh = false;
 
   @override
   Widget build(BuildContext context) {
-    if (listener != null) {
-      widgetsBinding.addPostFrameCallback((v) {
-        var _listener = listener;
-        listener = null;
-        _listener();
-      });
-    }
-    if (_forcedRefresh) return Text("");
-    if (widget.hide) return Offstage(offstage: true);
-    var w = widget.child;
-    if (widget.widgetBuilder != null) w = widget.widgetBuilder(context);
-    if (widget.opacity < 1) w = Opacity(child: w, opacity: widget.opacity);
-    if (widget.onTap != null)
-      w = GestureDetector(child: w, onTap: widget.onTap);
-    if (widget.disableBack) {
-      w = WillPopScope(
-          child: w,
-          onWillPop: () {
-            return Future.value(false);
-          });
-    }
-    return w;
+    return widget.child;
   }
 
   @override
   void initState() {
     widgetsBinding = WidgetsBinding.instance;
-    if (widget.controller != null) {
-      widget.controller._state = this;
-      widget.controller._context = context;
-    }
-    Common.printLog("key${widget.key.toString()}创建");
+    widget.controller._state = this;
+    widget.controller._context = context;
+    widgetsBinding.addPostFrameCallback((v){
+      widget.controller.frameCallback(context,v);
+    });
   }
 
   @override
   void dispose() {
     super.dispose();
+    WidgetsBinding.instance.removeObserver(this); //销毁观察者
   }
 }
 
 class StatefulWidgetController {
   _StatefulWidgetCellStatePlus _state;
   BuildContext _context;
+  Function frameCallback;
+
+  StatefulWidgetController(@required this.frameCallback);
 
   BuildContext getContext() {
     return _context;
@@ -83,23 +58,4 @@ class StatefulWidgetController {
     return _state;
   }
 
-  void dispatch([VoidCallback listener = null]) {
-    if (_state != null) {
-      _state.listener = listener;
-      if (_state.mounted) _state.setState(() {});
-    }
-  }
-
-  void forcedUnload([VoidCallback back]) {
-    _state._forcedRefresh = true;
-    dispatch(() {
-      _state._forcedRefresh = false;
-      if (back != null) back();
-    });
-  }
-
-  void dispose() {
-    if (_state != null) _state.dispose();
-    _state = null;
-  }
 }
