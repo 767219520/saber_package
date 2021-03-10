e68b512274f0fe61635f733b7547d943ebecc458
jianggengwu <767219520@qq.com>
2021-03-10 11:57:11 +0800

    1

--- lib/src/utils/time_utils.dart
+++ lib/src/utils/time_utils.dart
@@ -1,9 +1,9 @@
 class TimeUtils {
   static void setTimeOutSeconds(int seconds, Function f) {
-    return setTimeOut(Duration(seconds: seconds), f);
+     setTimeOut(Duration(seconds: seconds), f);
   }
 
-  static int setTimeOut(Duration d, Function f, [int loopCount = 1 ]) {
+  static void setTimeOut(Duration d, Function f, [int loopCount = 1 ]) {
     if (f == null || loopCount <= 0) return 0;
     Future.delayed(d).then((value) {
       f();
