29bebe5fa78621b34a04fc5fdb6065b3e33159ed
jianggengwu <767219520@qq.com>
2021-03-11 15:33:36 +0800

    1

--- lib/src/tools/if_cell_tools.dart
+++ lib/src/tools/if_cell_tools.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/cupertino.dart';
 
 class IfCellState {
-  static IfCellEntity defaultEntity=new IfCellEntity(true,null);
+  static IfCellEntity defaultEntity = new IfCellEntity(true, null);
 
-  static dynamic getResult(List<IfCellEntity> ifs) {
-    IfCellEntity ifCellEntity= ifs.firstWhere((element) => element.expression)??defaultEntity;
-    return ifCellEntity.cell;
+  static Widget getResult(List<IfCellEntity> ifs, BuildContext context) {
+    IfCellEntity ifCellEntity =
+        ifs.firstWhere((element) => element.expression) ?? defaultEntity;
+    return ifCellEntity.cell(context);
   }
 }
 
 class IfCellEntity {
   bool expression;
-  dynamic cell;
+  WidgetBuilder cell;
 
   IfCellEntity(this.expression, this.cell);
 }
